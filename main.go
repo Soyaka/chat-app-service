6452c3db695cb5a868dab03f7dac39d7cd18359f
Soyaka <soyaka@soyaka.soyaka>
2024-03-26 23:13:31 +0000

    database mockup

--- main.go
+++ main.go
@@ -15,13 +15,13 @@ func main() {
 	})
 
 	http.HandleFunc("/register", func(w http.ResponseWriter, r *http.Request) {
-
+			
 	})
 
 	http.HandleFunc("/login", func(w http.ResponseWriter, r *http.Request) {
 		
 	})
-	
+
 	http.ListenAndServe(":4444", nil)
 	wg.Wait()
 }
--- makefile
+++ makefile
@@ -1,2 +1,6 @@
 run:
 	@go run main.go
+push:
+	git add .
+	git commit -m "$(m)"
+	git push origin master
